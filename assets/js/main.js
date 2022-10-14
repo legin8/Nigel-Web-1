e36748990846d96c1e7c8ee8c84e01fe28bfe481
legin8 <maynne1@student.op.ac.nz>
2022-10-14 17:39:42 +1300

    progress on the js stuff for selecting the divs I want, but still have proplems

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,5 +3,9 @@ document.querySelector(".blockB").addEventListener("click", e => {
   alert(part2);
   
   let part1 = document.querySelector(".block6Part1");
-  
 });
+
+
+let part1 = document.querySelector(".block6Part1").style.display = "none";
+let part2 = document.querySelector(".block6Part2").style.display = "block";
+document.querySelector(".block6VerticalBlock.blockB").style.width = "7%";
