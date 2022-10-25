12232d3097a7323ce25370dfaea6b673d3aa29f3
Nigel Maynard (1000018419) <MAYNNE1@student.op.ac.nz>
2022-10-25 14:26:15 +1300

    Calling api images on 3 pitures on my page, 1 isn't working

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,18 +1,25 @@
-let allImgs = document.querySelectorAll("img");
+let allImgs = document.querySelectorAll(".imgToApi");
 console.log(allImgs);
 console.log(allImgs.length);
 
 const BASE_URL = "https://api.unsplash.com";
 const API_KEY = "cm4Mq6siY-LIWX3qZD3M6SU1t2Z3qoix0v8ObeVJHOc";
-let picuturesToGet = allImgs.length;
 
-fetch(`${BASE_URL}/search/photos?query=coffee&per_page=${picuturesToGet}&client_id=${API_KEY}`)
+fetch(`${BASE_URL}/search/photos?query=coffee&per_page=${allImgs.length}&client_id=${API_KEY}`)
 .then(response => response.json())
 .then(data => {
   allImgs.forEach((img, i) => {
-    img.src = data.results[i].urls.regular;
+    img.style.backgroundImage = `url(${data.results[i].urls.regular})`;
+    console.log(data.results[i].urls.regular);
     });
 });
+
+
+
+
+
+
+
 
 let block6Panel1 = document.querySelector(".block6Part1");
 let block6Panel2 = document.querySelector(".block6Part2")
